c505c0b4f63c4071dc0d797ee77bbab402a10116
audreywl <audrey.lewis@students.olin.edu>
2017-01-29 18:15:24 -0500

    done with marker code

--- warmup_package/scripts/marker.py
+++ warmup_package/scripts/marker.py
@@ -10,29 +10,30 @@ import rospy
 class Sphero(object):
     def __init__(self):
         rospy.init_node('marker')
-        rospy.Publisher('/visualization_marker', Marker, queue_size=10)
+        self.pub = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
         self.r = rospy.Rate(10)
         self.spherey = Marker()
-        self.spherey.header.frame_id = "/Cmap"
+        self.spherey.header.frame_id = "/map"
         self.spherey.header.stamp    = rospy.get_rostime()
         self.spherey.ns = "robot"
         self.spherey.id = 0
         self.spherey.type = 2 # sphere
         self.spherey.action = 0
-        self.spherey.pose.position = self.state.point
-        self.spherey.pose.orientation.x = 0
-        self.spherey.pose.orientation.y = 0
-        self.spherey.pose.orientation.z = 0
-        self.spherey.pose.orientation.w = 1.0
-        self.spherey.scale.x = 1.0
-        self.spherey.scale.y = 1.0
-        self.spherey.scale.z = 1.0
+        self.spherey.pose.position.x = 1.0
+        self.spherey.pose.position.y = 2.0
+        self.spherey.scale.x = 0.5
+        self.spherey.scale.y = 0.5
+        self.spherey.scale.z = 0.5
 
         self.spherey.color.r = 0.0
         self.spherey.color.g = 1.0
-        self.spherey.color.b = 0.0
+        self.spherey.color.b = 1.0
         self.spherey.color.a = 1.0
     def run(self):
         while not rospy.is_shutdown():
             self.r.sleep()
+            self.pub.publish(self.spherey)
         print "Node is finished!"
+
+test_marker=Sphero()
+test_marker.run()
