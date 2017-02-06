422fe00fdeb954b7ed93635dc07c92c8ddb3a34d
audreywl <audrey.lewis@students.olin.edu>
2017-02-06 17:05:09 -0500

    marker and commenting aaaalmost done

--- warmup_package/scripts/wall_follow.py
+++ warmup_package/scripts/wall_follow.py
@@ -20,21 +20,24 @@ def convert_pose_to_xy_and_theta(msg):
     return (pose.position.x, pose.position.y, angles[2])
 
 class Sphero(object):
+    """Marker Class - contains a spherical marker and info for publishing it"""
     def __init__(self):
+        #make publisher
         self.pub = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
-        self.r = rospy.Rate(10)
         self.spherey = Marker()
         self.spherey.header.frame_id = "/odom"
         self.spherey.header.stamp    = rospy.get_rostime()
         self.spherey.ns = "robot"
         self.spherey.id = 0
-        self.spherey.type = 2 # sphere
+        self.spherey.type = 2 # this is what makes it a sphere
         self.spherey.action = 0
-        self.spherey.pose.position.x = 1.0
-        self.spherey.pose.position.y = 2.0
-        self.spherey.scale.x = 0.5
-        self.spherey.scale.y = 0.5
-        self.spherey.scale.z = 0.5
+        #Sphere Attributes
+        self.spherey.pose.position.x = 0.0
+        self.spherey.pose.position.y = 0.0
+
+        self.spherey.scale.x = 0.2
+        self.spherey.scale.y = 0.2
+        self.spherey.scale.z = 0.2
 
         self.spherey.color.r = 0.0
         self.spherey.color.g = 1.0
@@ -57,68 +60,69 @@ class WallFollower(object):
         rospy.Subscriber('/odom', Odometry, self.processOdom)
 
     def make_marker(self,polar_position):
+        #convert target position from polar
         delta_y = polar_position[0]*math.sin(polar_position[1])
         delta_x= polar_position[0]*math.cos(polar_position[1])
-        # print 'x:'
-        # print str(delta_x)
-        # print 'y:'
-        # print str(delta_y)
-        self.marker.spherey.pose.position.x = self.pos.x-delta_x
-        self.marker.spherey.pose.position.y = self.pos.y+delta_y
+        #add to current position
+        self.marker.spherey.pose.position.x = self.pos.x+delta_y
+        self.marker.spherey.pose.position.y = self.pos.y-delta_x
+
     def processOdom(self,msg):
         self.pos.x, self.pos.y, self.pos.z = convert_pose_to_xy_and_theta(msg)
-        #self.marker.spherey.pose.position.x = self.pos.x
-        #self.marker.spherey.pose.position.y = self.pos.y
+
     def process_scan(self,msg):
+        """Looks for wall in the 40degree range in front of the bot"""
         self.wall = []
         self.see_wall = False
         for i in range(0,20):
             dist = msg.ranges[i]
-            if dist == 0.0:
+            if dist == 0.0: #0.0 just means it reads nothing, not something close
                 continue
             elif dist < 1.0:
                 self.see_wall = True
-                self.wall.append([dist, i])
+                self.wall.append([dist, i*pi/180])
         for i in range(340,360):
             dist = msg.ranges[i]
             if dist == 0.0:
                 continue
             elif dist < 1.0:
                 self.see_wall = True
-                self.wall.append([dist, i])
-        #print str(self.see_wall)
+                self.wall.append([dist, (i-360)*pi/180]) #make ranges go from -40 to 40 instead of 0 to 40 and 320 to 360 and then converts to radians
+
         if self.see_wall:
-            self.wall.sort(key=lambda lst: lst[0])
-            self.make_marker(self.wall[0])
-            self.direction = 1
-            if self.wall[0][1] > 180:
-                #self.wall[0][0] *= -1
+            self.wall.sort(key=lambda lst: lst[0]) #sort by nearest point
+
+            self.make_marker([self.wall[0][0],self.wall[0][1]]) #pass along nearest point to marker
+            if self.wall[0][1] < 0.0: #set which direction to turn
+                self.direction = 1
+            else:
                 self.direction = -1
 
 
 
     def process_bump(self,msg):
+        """Detects bump on any side"""
         if (msg.leftFront==1 or msg.leftFront==1 or msg.rightFront ==1 or msg.rightSide==1):
-            print 'BUMP!'
+            print 'ouch!'
             self.is_bumped = True
 
         else:
 			self.is_bumped = False
     def fucking_stop(self):
-        print 'STOP!'
+        """Emergency stop function, publishs itself immediately"""
+        print "I'm so done with this"
         self.moves.linear.x = 0.0
         self.moves.angular.z = 0.0
         self.pub.publish(self.moves)
+
     def run(self):
-        rospy.on_shutdown(self.fucking_stop)
+        rospy.on_shutdown(self.fucking_stop) #makes sure the robot stops when the code shuts down
         while not rospy.is_shutdown():
-            #print 'running!'
-            if self.is_bumped:
-                self.fucking_stop()
+            if self.is_bumped: #if bumped, stop everything and quit running
+
                 break
             elif self.see_wall:
-                #self.moves.linear.x = -.1
-                self.moves.angular.z = -.2/(self.wall[0][0])*self.direction
+                self.moves.angular.z = .2/(self.wall[0][0])*self.direction
             else:
                 self.moves.linear.x = .1
                 self.moves.angular.z = 0.0
@@ -127,7 +131,6 @@ class WallFollower(object):
             self.marker.pub.publish(self.marker.spherey)
             self.r.sleep()
         print "Node is finished!"
-        self.fucking_stop()
 
 FollowIt = WallFollower()
 FollowIt.run()
